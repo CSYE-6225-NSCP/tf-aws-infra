9a149f90a6f7507303625db49aa8ef03e3efa786
malepatic <malepati.c@northeastern.edu>
2025-03-25 19:26:32 -0400

    added the policy and update the userdata

--- dev-infra/iam.tf
+++ dev-infra/iam.tf
@@ -18,6 +18,11 @@ resource "aws_iam_role_policy_attachment" "s3_access" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
+resource "aws_iam_role_policy_attachment" "cloudwatch_agent" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "ec2-instance-profile"
   role = aws_iam_role.ec2_role.name
