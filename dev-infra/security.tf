7d61d6ca5010a6ec18c504861e274a2676cc953a
malepatic <malepati.c@northeastern.edu>
2025-03-31 22:23:38 -0400

    removed the ipv6 in the loadbalancer

--- dev-infra/security.tf
+++ dev-infra/security.tf
@@ -31,20 +31,6 @@ resource "aws_security_group" "lb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -52,12 +38,6 @@ resource "aws_security_group" "lb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    ipv6_cidr_blocks = ["::/0"]
-  }
 
   tags = {
     Name = "LoadBalancerSecurityGroup"
