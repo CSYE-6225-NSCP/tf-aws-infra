dddb1f814cd24e6d5648c3b9c10e4a4a9dd89ae7
malepatic <malepati.c@northeastern.edu>
2025-04-12 03:07:00 -0400

    modified password generation

--- dev-infra/asg-policy.tf
+++ dev-infra/asg-policy.tf
@@ -31,7 +31,7 @@ resource "aws_autoscaling_policy" "scale_down" {
   name                   = "scale_down"
   scaling_adjustment     = var.scaling-adjustment-down
   adjustment_type        = "ChangeInCapacity"
-  cooldown               = 60
+  cooldown               = var.cooldown-period
   autoscaling_group_name = aws_autoscaling_group.web_app_asg.name
 }
 
